4e5e403e49792a2c68332b07628c3f896095b768
Antonius131 <antomendetta@gmail.com>
2022-07-04 15:20:33 +0200

    bonus: add image in DOM

--- js/script.js
+++ js/script.js
@@ -3,6 +3,7 @@ const app = new Vue (
       el: root,
       data: {
          headTitle: 'Hello Vue!',
+         imgSrc: 'https://cutewallpaper.org/22/minimal-programming-wallpapers/739725025.jpg'
       }
    }
 );
